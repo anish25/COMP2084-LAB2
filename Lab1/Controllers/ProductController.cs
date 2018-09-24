d96030e441e3e917b1b2a0d701b3f67dab7489d3
anish25 <anishgandhi25@gmail.com>
2018-09-24 07:41:08 -0400

    LAB2 COMPLETED SUCESSFULLY

---
+++ Lab1/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Get Product/Details
+        public ActionResult details(string ProductName)
+        {
+            ViewBag.ProductName = ProductName;
+            return View();
+        }
+    }
+
+    
+ }
